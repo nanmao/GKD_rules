03bf4f5cb903cfd1659df2bb2484f5faedcbabe0
nanmao <nanmaomaomao@gmail.com>
2026-07-22 09:09:14 +0800

    fix: change checkUpdateUrl to absolute URL

--- src/subscription.ts
+++ src/subscription.ts
@@ -8,9 +8,11 @@ export default defineGkdSubscription({
   name: 'oppo_gkd_subscription_rules',
   version: 0,
   author: '夏初临',
+
   updateUrl:
     'https://raw.githubusercontent.com/nanmao/GKD_rules/main/dist/gkd.json5',
-  checkUpdateUrl: './gkd.version.json5',
+  checkUpdateUrl:
+    'https://raw.githubusercontent.com/nanmao/GKD_rules/main/dist/gkd.version.json5',
   supportUri: 'https://github.com/gkd-kit/subscription-template',
   categories,
   globalGroups,
