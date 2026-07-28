8a6a1b6b34037e640afce615a21d6b230ad05e99
nanmao <nanmaomaomao@gmail.com>
2026-07-28 10:28:46 +0800

    优化百度广告关闭

--- src/apps/com.coloros.weather2.ts
+++ src/apps/com.coloros.weather2.ts
@@ -7,7 +7,7 @@ export default defineGkdApp({
     {
       key: 0,
       name: '功能类-关闭广告',
-      desc: '点击天气首页的广告关闭按钮',
+      desc: '点击天气首页的广告关闭按钮，再点击标题党',
       matchTime: 10000,
       actionMaximum: 1,
       resetMatch: 'match',
@@ -18,6 +18,14 @@ export default defineGkdApp({
           name: 'feed_close',
           fastQuery: true,
           matches: '[vid="feed_close"]',
+          action: 'clickCenter',
+        },
+        {
+          key: 1,
+          name: '标题党',
+          preKeys: [0],
+          actionDelay: 100,
+          matches: '[text="标题党"]',
           action: 'clickCenter',
         },
       ],
