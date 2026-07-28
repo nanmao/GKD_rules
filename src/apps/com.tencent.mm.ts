c85496f35c2296b577e65e0bde8820b1fa973808
nanmao <nanmaomaomao@gmail.com>
2026-07-28 14:54:58 +0800

    优化微信小程序跳过规则

--- src/apps/com.tencent.mm.ts
+++ src/apps/com.tencent.mm.ts
@@ -8,8 +8,6 @@ export default defineGkdApp({
       key: 0,
       name: '开屏广告-小程序跳过',
       desc: '点击小程序中的跳过/关闭按钮',
-      matchTime: 10000,
-      actionMaximum: 1,
       resetMatch: 'match',
       rules: [
         {
@@ -21,7 +19,8 @@ export default defineGkdApp({
         {
           key: 1,
           name: '跳过倒计时（图片按钮）',
-          matches: '[desc*="跳过"||desc*="skip"][visibleToUser=true]',
+          matches:
+            '[desc*="跳过"||desc*="skip"][visibleToUser=true][width<200][height<200]',
           action: 'clickCenter',
         },
       ],
@@ -30,14 +29,12 @@ export default defineGkdApp({
       key: 1,
       name: '局部广告-小程序页面横幅',
       desc: '点击小程序-台风路径页面中的广告关闭按钮',
-      matchTime: 10000,
-      actionMaximum: 1,
       resetMatch: 'match',
       rules: [
         {
           key: 0,
           name: '特殊字符关闭',
-          matches: '[text=""]',
+          matches: '[text=""][visibleToUser=true]',
           action: 'clickCenter',
         },
       ],
@@ -46,14 +43,13 @@ export default defineGkdApp({
       key: 2,
       name: '局部广告-小程序悬浮广告',
       desc: '点击小程序-台风路径中的悬浮广告关闭按钮',
-      matchTime: 10000,
-      actionMaximum: 1,
       resetMatch: 'match',
       rules: [
         {
           key: 0,
           name: '悬浮广告关闭',
-          matches: '@ImageView[width=49][height=49] <<n [vid="w_"]',
+          matches:
+            '@ImageView[visibleToUser=true][width=49][height=49] <<n [vid="w_"]',
           action: 'clickCenter',
           snapshotUrls: 'https://i.gkd.li/i/30439674',
         },
